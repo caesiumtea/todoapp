7066d9b066c62c15c5a4a7db06129dc06f81501c
Vance Moss <caesiumtea@gmail.com>
2020-07-01 22:13:26 -0400

    Set up database connection

--- todo.py
+++ todo.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://vance@localhost:5432/todoapp"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = "todos"
+    id = db.Column(db.Integer, primary_key=True)
+    info = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f"<Todo {self.id}: {self.info}>"
+
+db.create_all()
 
 #route handler for index (root) page of site
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        "info":"Item 1"
-        }, {
-        "info":"Item 2"
-        }, {
-        "info":"Item 3"
-    }]) #this is dummy data currently, will replace with a list of objects
-        #pulled from database by SQLAlchemy
+    return render_template('index.html', data=Todo.query.all())
